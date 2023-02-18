eb7dab3bf5b0dfb608e9d141f2ee330bc0fb2617
priyab99 <priya.bd.9911@gmail.com>
2023-02-18 21:54:19 +0600

    random colors

--- js/randomColor.js
+++ js/randomColor.js
@@ -1,16 +1,10 @@
 
 
-document.getElementById('card').addEventListener('click',function(){
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  card.style.backgroundColor = "#" + randomColor;
-  color.innerHTML = "#" + randomColor;
+document.getElementById('card1').addEventListener('click',function(){
+    const color= document.getElementById('card1');
+    
 })
 
-document.getElementById('card2').addEventListener('click',function(){
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  card.style.backgroundColor = "#" + randomColor;
-  color.innerHTML = "#" + randomColor;
-})
 
 
 
